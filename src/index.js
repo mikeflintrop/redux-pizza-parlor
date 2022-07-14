c75341b764c0382f26d5f1602f6c92d01a9bf05c
Mike Flintrop <mike.flintrop@gmail.com>
2022-07-14 11:46:09 -0500

    updated index

--- src/index.js
+++ src/index.js
@@ -3,4 +3,58 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+// Reducer Pizza List
+const pizzaListReducer = (state = [], action) => {
+    console.log('Reducer pizzalist: ', action)
+    
+    switch (action.type) {
+    
+        case 'SET_PIZZA_LIST':
+            console.log('You clicked SET_PIZZA_LIST', action.payload);
+        return action.payload;
+    
+        default: 
+        return state;
+    }
+}
+
+// Reducer pizzas in the cart
+const cartReducer = (state = [], action) => {
+    console.log('Reducer cart', action);
+
+    switch (action.type) {
+        case 'ADD_TO_CART':
+            console.log('You clicked ADD_TO_CART', action.payload);
+        return action.payload;
+
+        case 'CLEAR_CART':
+            console.log('You clicked checkout CLEAR_CART', action.payload);
+        return action.payload;
+        
+        default: 
+        return state;
+        
+    }
+};
+    
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+    combineReducers({
+    pizzaListReducer,
+    cartReducer,
+
+    }),
+    applyMiddleware(logger),
+);
+
+// Wrap our App in a Provider, this makes Redux available in
+// our entire application
+ReactDOM.render(
+<Provider store={storeInstance}>
+    <App />
+</Provider>, document.getElementById('root'));
