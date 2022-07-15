14de1aaa6b834b68d192e6880bce73b0b13d1e6b
Isaac Brist <isaacbrist@gmail.com>
2022-07-15 10:34:46 -0500

    Fixed the ADD_tO_CART in the index. Properly adds items to the state.

--- src/index.js
+++ src/index.js
@@ -10,72 +10,86 @@ import {Provider} from 'react-redux';
 
 // Reducer Pizza List
 const pizzaListReducer = (state = [], action) => {
-    console.log('Reducer pizzalist: ', action);
+  console.log('Reducer pizzalist: ', action);
 
-    switch (action.type) {
+  switch (action.type) {
     case 'SET_PIZZA_LIST':
-        console.log('You clicked SET_PIZZA_LIST', action.payload);
-        return action.payload;
+      console.log('You clicked SET_PIZZA_LIST', action.payload);
+      return action.payload;
 
     default:
-        return state;
-    }
+      return state;
+  }
 };
 
 // Customer Reducer
 const customerTableReducer = (state = [], action) => {
-    console.log('Customer Table reducer: ', action);
+  console.log('Customer Table reducer: ', action);
 
-    switch (action.type) {
-        case 'ADD_CUSTOMER':
-        console.log('You clicked ADD_CUSTOMER', action.payload);
+  switch (action.type) {
+    case 'ADD_CUSTOMER':
+      console.log('You clicked ADD_CUSTOMER', action.payload);
       // state.push(action.payload); -- wrong
       // spread operator allows new array to be passed with action.payload
-        return action.payload;
+      return action.payload;
 
     default:
       // if action.type is anything else, return last value of state
-        return state;
-    }
+      return state;
+  }
 };
-
+// const [updateCart, setUpdateCart]=useState(true)
+let totalCost = 0;
+let cartItems = [];
+let itemsCount;
 // Reducer pizzas in the cart
+// {totalCost: 0, cartItems: [], itemsCount: 0}
 const cartReducer = (state = [], action) => {
-    console.log('Reducer cart', action);
+  console.log('Reducer cart', action);
 
-    switch (action.type) {
-        case 'ADD_TO_CART':
-            console.log('You clicked ADD_TO_CART', action.payload);
-        return action.payload;
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      console.log('You clicked ADD_TO_CART', action.payload);
 
-        case 'DELETE_FROM_CART':
-            console.log('You clicked DELETE_FROM_CART', action.payload);
-        return action.payload;
+      return [...state, action.payload];
+    // totalCost: (totalCost += Number(action.payload.price)),
 
-        case 'CLEAR_CART':
-            console.log('You clicked checkout CLEAR_CART', action.payload);
-        return action.payload;
 
-        default:
-        return state;
-    }
+    case 'DELETE_FROM_CART':
+      console.log('You clicked DELETE_FROM_CART', action.payload);
+      const pizzaSelected = (pizza) => {
+        pizza.id !== payload.id;
+        const updateCart = cartItems.filter(pizzaSelected);
+      };
+      return {
+        totalCost: (totalCost -= Number(action.payload.price)),
+        cartItems: updateCart,
+      };
+
+    case 'CLEAR_CART':
+      console.log('You clicked checkout CLEAR_CART', action.payload);
+      return action.payload;
+
+    default:
+      return state;
+  }
 };
 
 // The store is the big JavaScript Object that holds all of the information for our application
 const storeInstance = createStore(
-    combineReducers({
+  combineReducers({
     pizzaListReducer,
     cartReducer,
     customerTableReducer,
-    }),
-    applyMiddleware(logger)
+  }),
+  applyMiddleware(logger)
 );
 
 // Wrap our App in a Provider, this makes Redux available in
 // our entire application
 ReactDOM.render(
-    <Provider store={storeInstance}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
